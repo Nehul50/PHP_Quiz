dba02c03da31fbc3d972c096852884592e575028
nehul_patel <nehul50@gmail.com>
2020-07-31 19:56:15 +0530

    mjhgf

---
+++ app/src/main/java/com/example/phpquiz/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.phpquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        ImageView.startAnimation(animation);
+    }
+}
